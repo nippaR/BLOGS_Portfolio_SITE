f460651a6dfb6626c3b42c28e3231e7a5f35bf44
Thanuja Nipun <thanujanipun6@gmail.com>
2024-12-26 10:13:21 +0530

    Adding header and footer and update home page and app.jsx page

--- portfolio_site/src/Frontend/projectHshow.jsx
+++ portfolio_site/src/Frontend/projectHshow.jsx
@@ -1,30 +1,100 @@
 import React from "react";
-import { Box,IconButton,Grid, Typography } from "@mui/material";
-import Image from '../assets/OIS.png';
+import { Box,IconButton,Grid, Typography, Button, Stack } from "@mui/material";
+import Image from '../assets/Bitcoin.jpeg';
+import javaImage from '../assets/JAVA.jpg';
 import GitHubIcon from '@mui/icons-material/GitHub';
 
 function projectHshow() {
 
     return (
 
-        <Grid sx={{mt:6}} >
-            
-            <Box sx={{ width:"400px", height:"450px", borderRadius: '15px', boxShadow: 15}} >
+        <Grid sx={{mt:4}} >
+           <Stack direction= 'row' sx={{gap:10, ml:3.5}}> 
+            {/* First Card */}
+            <Box sx={{ width:"250px", height:"300px", borderRadius: '15px', boxShadow: 15, transition: 'transform 0.3s ease-in-out','&:hover': {transform: 'scale(1.1)'}}} >
                 <Grid sx={{ paddingTop:2, ml:3}} >
-                <img src={Image} alt="My Image" style={{width: "350px", height: '300px',
+                <img src={Image} alt="My Image" style={{width: "200px", height: '150px',
                                                         borderRadius: '15px',
-                                                        boxShadow: "0px 2px 10px rgba(0, 33, 251, 0.2)"}} />
+                                                        boxShadow: "0px 2px 10px rgba(251, 205, 0, 0.3)"}} />
 
                                                         
-                      <Typography variant="h6" sx={{mt:2, ml:8}} >
+                      <Typography sx={{mt:2, ml:2.5, fontSize: "0.9rem"}} >
                            Block Chain Application  
                       </Typography> 
-                      <IconButton aria-label="GitHub" sx={{mt:2,ml:18,color: 'white', scale: 1.7, ":hover": {color: "#1e1eff"}}}>
+                      <IconButton aria-label="GitHub" sx={{mt:2,ml:5,color: 'white', scale: 1.7, ":hover": {color: "#ffab10"}}}>
                         <GitHubIcon />
-                    </IconButton>   
+                    </IconButton>  
+                    <Button variant="contained" sx={{borderRadius:'8px',backgroundColor: "#1e1eff", ":hover": {backgroundColor: "#ffab10"}, mt:2.8, ml:2}}>  
+                        <Typography sx={{fontSize:"0.7rem", fontWeight:"bold"}}>View</Typography>
+                    </Button>
+                </Grid>
+            </Box>
+            
+            {/* Secound Card */}
+
+            <Box sx={{ width:"250px", height:"300px", borderRadius: '15px', boxShadow: 15, transition: 'transform 0.3s ease-in-out','&:hover': {transform: 'scale(1.1)'}}} >
+                <Grid sx={{ paddingTop:2, ml:3}} >
+                <img src={javaImage} alt="java Image" style={{width: "200px", height: '150px',
+                                                        borderRadius: '15px',
+                                                        boxShadow: "0px 2px 10px rgba(0, 251, 251, 0.3)"}} />
+
+                                                        
+                      <Typography sx={{mt:2, ml:1, fontSize: "0.9rem"}} >
+                           Student Management System  
+                      </Typography> 
+                      <IconButton aria-label="GitHub" sx={{mt:2,ml:5,color: 'white', scale: 1.7, ":hover": {color: "#04fde4"}}}>
+                        <GitHubIcon />
+                    </IconButton>  
+                    <Button variant="contained" sx={{borderRadius:'8px',backgroundColor: "#1e1eff", ":hover": {backgroundColor: "#04fde4"}, mt:2.8, ml:2}}>  
+                        <Typography sx={{fontSize:"0.7rem", fontWeight:"bold"}}>View</Typography>
+                    </Button>
                 </Grid>
             </Box>
 
+            {/* Third Card */}
+
+            <Box sx={{ width:"250px", height:"300px", borderRadius: '15px', boxShadow: 15, transition: 'transform 0.3s ease-in-out','&:hover': {transform: 'scale(1.1)'}}} >
+                <Grid sx={{ paddingTop:2, ml:3}} >
+                <img src={javaImage} alt="java Image" style={{width: "200px", height: '150px',
+                                                        borderRadius: '15px',
+                                                        boxShadow: "0px 2px 10px rgba(0, 251, 251, 0.3)"}} />
+
+                                                        
+                      <Typography sx={{mt:2, ml:2, fontSize: "0.9rem"}} >
+                           Docker Base Application 
+                      </Typography> 
+                      <IconButton aria-label="GitHub" sx={{mt:2,ml:5,color: 'white', scale: 1.7, ":hover": {color: "#04fde4"}}}>
+                        <GitHubIcon />
+                    </IconButton>  
+                    <Button variant="contained" sx={{borderRadius:'8px',backgroundColor: "#1e1eff", ":hover": {backgroundColor: "#04fde4"}, mt:2.8, ml:2}}>  
+                        <Typography sx={{fontSize:"0.7rem", fontWeight:"bold"}}>View</Typography>
+                    </Button>
+                </Grid>
+            </Box>
+            
+            {/* Fourth Card */}
+
+            <Box sx={{ width:"250px", height:"300px", borderRadius: '15px', boxShadow: 15, transition: 'transform 0.3s ease-in-out','&:hover': {transform: 'scale(1.1)'}}} >
+                <Grid sx={{ paddingTop:2, ml:3}} >
+                <img src={javaImage} alt="java Image" style={{width: "200px", height: '150px',
+                                                        borderRadius: '15px',
+                                                        boxShadow: "0px 2px 10px rgba(0, 251, 251, 0.3)"}} />
+
+                                                        
+                      <Typography sx={{mt:2, ml:4, fontSize: "0.9rem"}} >
+                        E-commerce System
+                      </Typography> 
+                      <IconButton aria-label="GitHub" sx={{mt:2,ml:5,color: 'white', scale: 1.7, ":hover": {color: "#04fde4"}}}>
+                        <GitHubIcon />
+                    </IconButton>  
+                    <Button variant="contained" sx={{borderRadius:'8px',backgroundColor: "#1e1eff", ":hover": {backgroundColor: "#04fde4"}, mt:2.8, ml:2}}>  
+                        <Typography sx={{fontSize:"0.7rem", fontWeight:"bold"}}>View</Typography>
+                    </Button>
+                </Grid>
+            </Box>
+
+
+          </Stack>
 
         </Grid>
 
